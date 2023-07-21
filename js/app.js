421fd939843dcac1787e1a3a6e2c01858feb10da
Vladislav <99101070+Vla9ik@users.noreply.github.com>
2023-07-22 00:03:21 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -868,6 +868,25 @@
                 FLS(`[Форми]: ${message}`);
             }
         }
+        function formQuantity() {
+            document.addEventListener("click", (function(e) {
+                let targetElement = e.target;
+                if (targetElement.closest("[data-quantity-plus]") || targetElement.closest("[data-quantity-minus]")) {
+                    const valueElement = targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]");
+                    let value = parseInt(valueElement.value);
+                    if (targetElement.hasAttribute("data-quantity-plus")) {
+                        value++;
+                        if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) value = valueElement.dataset.quantityMax;
+                    } else {
+                        --value;
+                        if (+valueElement.dataset.quantityMin) {
+                            if (+valueElement.dataset.quantityMin > value) value = valueElement.dataset.quantityMin;
+                        } else if (value < 1) value = 1;
+                    }
+                    targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]").value = value;
+                }
+            }));
+        }
         class SelectConstructor {
             constructor(props, data = null) {
                 let defaultConfig = {
@@ -4695,6 +4714,7 @@
             autoHeight: false
         });
         formSubmit();
+        formQuantity();
         pageNavigation();
         headerScroll();
     })();
